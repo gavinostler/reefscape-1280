b0f277bd6c36a25fd066e34600d7a6c315b4105c
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2024-11-09 19:36:35 -0800

    Feat: Swerve Constants

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -25,7 +25,7 @@ public class ExampleSubsystem extends SubsystemBase {
         });
   }
 
-  /**
+  /*q*
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -1,13 +1,9 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.PointWheelsAt;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.RobotCentric;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Drivetrain;
 
@@ -17,11 +13,12 @@ public class SwerveDriveSubsystem extends SubsystemBase {
   final PointWheelsAt m_pointWheelsAtRequest = new PointWheelsAt();
   final RobotCentric m_robotCentricRequest = new RobotCentric();
   private double angle = 0.0;
+
   @Override
   public void periodic() {
-    angle += 5.0;
-    Rotation2d rotation = new Rotation2d(angle);
-    Drivetrain.swerveDrivetrain.setControl(m_pointWheelsAtRequest.withModuleDirection(rotation));
-    Drivetrain.swerveDrivetrain.setControl(m_robotCentricRequest.withVelocityX(0.2));
+    //angle += 5.0;
+    //Rotation2d rotation = new Rotation2d(angle);
+    //Drivetrain.swerveDrivetrain.setControl(m_pointWheelsAtRequest.withModuleDirection(rotation));
+    //Drivetrain.swerveDrivetrain.setControl(m_robotCentricRequest.withVelocityX(0.2));
   }
 }
