8928abec44a3838c9356b777827e4f568d5017b7
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2024-11-21 15:53:39 -0800

    optimize movement

--- src/main/java/frc/robot/commands/SwerveMovementCommand.java
+++ src/main/java/frc/robot/commands/SwerveMovementCommand.java
@@ -16,28 +16,29 @@ public class SwerveMovementCommand extends Command {
     private final SwerveDriveSubsystem m_swerve; 
     private PhoenixPIDController m_thetaController;
 
-    private final DoubleSupplier x_velocity_supplier;
-    private final DoubleSupplier y_velocity_supplier;
+    private final DoubleSupplier x_supplier;
+    private final DoubleSupplier y_supplier;
     private final DoubleSupplier rot_supplier;
 
-    private double x_velocity, y_velocity, rot;
+    private double x_val, y_val, rot_val;
     private SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     private SwerveRequest.FieldCentricFacingAngle driveAngle = new SwerveRequest.FieldCentricFacingAngle();
     private SwerveRequest request;
 
     private double speedMultiplier = 0.5;
     private double angularSpeedMultiplier = 0.8;
+    private double driveDeadband = 0.1;
 
     public SwerveMovementCommand(SwerveDriveSubsystem swerve,
-        DoubleSupplier x_velocity_supplier,
-        DoubleSupplier y_velocity_supplier,
+        DoubleSupplier x_supplier,
+        DoubleSupplier y_supplier,
         DoubleSupplier rot_supplier
     ) {
         this.addRequirements(swerve);
 
         this.m_swerve = swerve;
-        this.x_velocity_supplier = x_velocity_supplier;
-        this.y_velocity_supplier = y_velocity_supplier;
+        this.x_supplier = x_supplier;
+        this.y_supplier = y_supplier;
         this.rot_supplier = rot_supplier;
     }
 
@@ -48,20 +49,30 @@ public class SwerveMovementCommand extends Command {
         driveAngle.HeadingController = m_thetaController;
     }
 
+    public void handleInput() {
+        this.rot_val = MathUtil.applyDeadband(rot_supplier.getAsDouble() * this.angularSpeedMultiplier, 0.1);
+        double x = x_supplier.getAsDouble();
+        double y = y_supplier.getAsDouble(); 
+
+        // deadband drive vector 
+        double magnitude = MathUtil.applyDeadband(Math.sqrt(x*x + y*y), driveDeadband);
+        if (magnitude == 0.0) {
+            this.x_val = 0.0;
+            this.y_val = 0.0;
+        } else {
+            this.x_val = x / Math.abs(x) * MathUtil.interpolate(0, this.speedMultiplier, Math.abs(x));
+            this.y_val = y / Math.abs(y) * MathUtil.interpolate(0, this.speedMultiplier, Math.abs(y));
+        }
+    }
+
     @Override
     public void execute() {
-        this.x_velocity = MathUtil.applyDeadband(x_velocity_supplier.getAsDouble() * this.speedMultiplier, 0.1);
-        this.y_velocity = MathUtil.applyDeadband(y_velocity_supplier.getAsDouble() * this.speedMultiplier, 0.1);
-        this.rot = MathUtil.applyDeadband(rot_supplier.getAsDouble() * this.angularSpeedMultiplier, 0.1);
-
-        SmartDashboard.putNumber("x_vel", x_velocity);
-        SmartDashboard.putNumber("y_vel", y_velocity);
-        SmartDashboard.putNumber("rot", rot);
+        handleInput();
 
         this.request = drive
-            .withVelocityX(this.x_velocity * Drivetrain.MAX_DRIVE_VOLTAGE)
-            .withVelocityY(this.y_velocity * Drivetrain.MAX_DRIVE_VOLTAGE)
-            .withRotationalRate(this.rot * Drivetrain.MAX_TURN_VOLTAGE);
+            .withVelocityX(this.x_val * Drivetrain.MAX_DRIVE_VOLTAGE)
+            .withVelocityY(this.y_val * Drivetrain.MAX_DRIVE_VOLTAGE)
+            .withRotationalRate(this.rot_val * Drivetrain.MAX_TURN_VOLTAGE);
 
         this.m_swerve.setControl(request);
     }
